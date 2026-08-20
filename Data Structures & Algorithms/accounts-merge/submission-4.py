9db541a1dfe95b1d599f81f517dbe00c03f57287
Wilfred Ting <88620367+wilfredting@users.noreply.github.com>
2026-08-19 21:48:48 -0700

    Add: accounts-merge - submission-4

---
+++ Data Structures & Algorithms/accounts-merge/submission-4.py
@@ -0,0 +1,56 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = {}
+        self.rank = {}
+
+        for i in range(n):
+            self.parent[i] = i
+            self.rank[i] = 1
+
+    def find(self, n):
+        while n != self.parent[n]:
+            self.parent[n] = self.parent[self.parent[n]]
+            n = self.parent[n]
+        return n
+
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2:
+            return False
+        
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+        elif self.rank[p2] > self.rank[p1]:
+            self.parent[p1] = p2
+        else:
+            self.parent[p1] = p2
+            self.rank[p2] += 1
+        
+        return True
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind(len(accounts))
+        emailToAcc = {}
+
+        for i, emails in enumerate(accounts):
+            for email in emails[1:]:
+                if email in emailToAcc:
+                    uf.union(i, emailToAcc[email])
+                else:
+                    emailToAcc[email] = i
+
+        accEmails = defaultdict(list)
+        for email, i in emailToAcc.items():
+            parent = uf.find(i)
+            accEmails[parent].append(email)
+
+        res = []
+        for i, emails in accEmails.items():
+            res.append([accounts[i][0]] + sorted(emails))
+
+        return res
+        
+
+
+        
