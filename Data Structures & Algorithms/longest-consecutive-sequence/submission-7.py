cc402499432f1ea0dfe547d2c7df82d53b148ea3
Wilfred Ting <88620367+wilfredting@users.noreply.github.com>
2026-08-20 21:56:12 -0700

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.py
@@ -0,0 +1,52 @@
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+        self.rank = {}
+        self.longest = 0
+
+    def add(self, n):
+        if n in self.parent:
+            return
+        self.parent[n] = n
+        self.rank[n] = 1
+        self.longest = max(self.longest, 1)
+
+    def find(self, n):
+        while n != self.parent[n]:
+            self.parent[n] = self.parent[self.parent[n]]
+            n = self.parent[n]
+        return n
+
+    def union(self, a, b):
+        pa, pb = self.find(a), self.find(b)
+        if pa == pb:
+            return False
+
+        if self.rank[pa] > self.rank[pb]:
+            self.parent[pb] = pa
+            self.rank[pa] += self.rank[pb]
+            self.longest = max(self.longest, self.rank[pa])
+        else:
+            self.parent[pa] = pb
+            self.rank[pb] += self.rank[pa]
+            self.longest = max(self.longest, self.rank[pb])
+        
+        return True
+        
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set()
+        uf = UnionFind()
+
+        for num in nums:
+            uf.add(num)
+            if num - 1 in num_set:
+                uf.union(num - 1, num)
+            if num + 1 in num_set:
+                uf.union(num + 1, num)
+            num_set.add(num)
+            
+        return uf.longest
+
+        
